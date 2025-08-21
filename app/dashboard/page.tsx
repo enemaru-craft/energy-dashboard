073a25ac5e57dfa977adf36b68c15d7e2ca2209b
GoRuGoo <dachanyr8989@gmail.com>
2025-08-21 23:56:41 +0900

    feat: チーム名を追加

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,6 +10,18 @@ export default function DashboardPage() {
   return (
     <div className="w-full  font-3xl min-h-screen p-8 ">
       <h1 className="text-3xl font-bold mb-6">Energy Dashboard</h1>
+      <section className="flex w-full mb-6">
+        <div className="flex-1 flex justify-center">
+          <p className="border rounded-2xl bg-white text-5xl font-bold px-8 py-4">
+            Team寿司
+          </p>
+        </div>
+        <div className="flex-1 flex justify-center">
+          <p className="border rounded-2xl bg-white text-5xl font-bold px-8 py-4">
+            Team天ぷら
+          </p>
+        </div>
+      </section>
       <div className="flex gap-6">
         <section className="justify-center w-1/2 flex  border rounded-4xl bg-white shadow-2xl ">
           <div className="grid grid-cols-2 gap-6 auto-rows-fr  pb-6">
